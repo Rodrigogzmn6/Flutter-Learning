560732f1e69ca211ab7526075c1343b1c0853d70
Rodrigogzmn6 <rodrigoguzman6796@gmail.com>
2022-03-27 20:29:06 -0300

    Working with Containers

--- Section 04 - Mi Card/mi_card_flutter/lib/main.dart
+++ Section 04 - Mi Card/mi_card_flutter/lib/main.dart
@@ -3,13 +3,19 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.orange,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            color: Colors.blueAccent,
+            child: Text('This is a container'),
+            padding: EdgeInsets.fromLTRB(10.0, 15.0, 20.0, 25.0),
+            margin: EdgeInsets.fromLTRB(25.0, 20.0, 15.0, 10.0),
+          ),
+        ),
       ),
     );
   }
