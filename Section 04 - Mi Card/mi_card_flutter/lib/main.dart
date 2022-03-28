080fdeef6d86ad9882f5982d70bfa4dada5480ef
Rodrigogzmn6 <rodrigoguzman6796@gmail.com>
2022-03-28 12:26:11 -0300

    All components added

--- Section 04 - Mi Card/mi_card_flutter/lib/main.dart
+++ Section 04 - Mi Card/mi_card_flutter/lib/main.dart
@@ -7,11 +7,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.black54,
+        backgroundColor: Colors.blueGrey.shade900,
         body: SafeArea(
           child: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
+              //crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 CircleAvatar(
                   radius: 50.0,
@@ -33,6 +34,44 @@ class MyApp extends StatelessWidget {
                     color: Colors.orange,
                     fontSize: 15.0,
                     fontFamily: 'Source Sans Pro',
+                  ),
+                ),
+                SizedBox(height: 10.0,),
+                Container(
+                  color: Colors.orange,
+                  padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(
+                        Icons.phone,
+                        color: Colors.blueGrey.shade900,
+                      ),
+                      SizedBox(width: 20.0),
+                      Text(
+                        '+54 3865 405769',
+                        style: TextStyle(fontSize: 15.0, fontFamily: 'Source Sans Pro', color: Colors.blueGrey.shade900, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 10.0,),
+                Container(
+                  color: Colors.orange,
+                  padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(
+                        Icons.mail_rounded,
+                        color: Colors.blueGrey.shade900,
+                      ),
+                      SizedBox(width: 20.0),
+                      Text(
+                        'rodrigoguzman6796@gmail.com',
+                        style: TextStyle(fontSize: 15.0, fontFamily: 'Source Sans Pro', color: Colors.blueGrey.shade900, fontWeight: FontWeight.bold),
+                      ),
+                    ],
                   ),
                 ),
               ],
