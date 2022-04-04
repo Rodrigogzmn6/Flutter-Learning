aec0822d278390e75d2510dbd6393b294954dfdb
Rodrigogzmn6 <rodrigoguzman6796@gmail.com>
2022-04-04 15:32:48 -0300

    Reformatted to stable channel

--- Section 07 - Packages/xylophone/lib/main.dart
+++ Section 07 - Packages/xylophone/lib/main.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
-  runApp(const XylophoneApp());
+  runApp(XylophoneApp());
 }
 
 class XylophoneApp extends StatefulWidget {
-  const XylophoneApp({Key? key}) : super(key: key);
 
   @override
   State<XylophoneApp> createState() => _XylophoneAppState();
 }
 
 class _XylophoneAppState extends State<XylophoneApp> {
+
+  Expanded noteKey ({int noteNumber, Color keyColor}){
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            final player = AudioCache();
+            player.play('note$noteNumber.wav');
+          });
+        },
+        child: Container(
+          color: keyColor,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,13 +36,13 @@ class _XylophoneAppState extends State<XylophoneApp> {
         body: SafeArea(
           child: Column(
             children: [
-              TextButton(
-                onPressed: () {},
-                child: Container(
-                  color: Colors.red,
-                  height: 20.0,
-                ),
-              ),
+              noteKey(noteNumber: 1, keyColor: Colors.red),
+              noteKey(noteNumber: 2, keyColor: Colors.orange),
+              noteKey(noteNumber: 3, keyColor: Colors.yellow),
+              noteKey(noteNumber: 4, keyColor: Colors.green),
+              noteKey(noteNumber: 5, keyColor: Colors.cyan),
+              noteKey(noteNumber: 6, keyColor: Colors.blue),
+              noteKey(noteNumber: 7, keyColor: Colors.purple),
             ],
           ),
         ),
