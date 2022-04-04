6faaef86379ec7d85e34fb3bf7e4e227e1ccd5a3
Rodrigogzmn6 <rodrigoguzman6796@gmail.com>
2022-04-04 20:05:12 -0300

    Adding conditional statements

--- Section 08 - Modularising/quizzler/lib/main.dart
+++ Section 08 - Modularising/quizzler/lib/main.dart
@@ -30,21 +30,36 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
+  List<bool> answers = [false, true, true];
+  int questionNumber = 0;
+  Icon correct = const Icon(
+    Icons.check,
+    color: Colors.green,
+  );
+  Icon wrong = const Icon(
+    Icons.close,
+    color: Colors.red,
+  );
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
-        const Expanded(
+        Expanded(
           flex: 5,
           child: Padding(
-            padding: EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
-                style: TextStyle(
+                style: const TextStyle(
                   fontSize: 25.0,
                   color: Colors.white,
                 ),
@@ -67,13 +82,13 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                if (answers[questionNumber] == true) {
+                  scoreKeeper.add(correct);
+                } else {
+                  scoreKeeper.add(wrong);
+                }
                 setState(() {
-                  scoreKeeper.add(
-                    const Icon(
-                      Icons.check,
-                      color: Colors.green,
-                    ),
-                  );
+                  questionNumber++;
                 });
               },
             ),
@@ -94,13 +109,13 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                if (answers[questionNumber] == false) {
+                  scoreKeeper.add(correct);
+                } else {
+                  scoreKeeper.add(wrong);
+                }
                 setState(() {
-                  scoreKeeper.add(
-                    const Icon(
-                      Icons.close,
-                      color: Colors.red,
-                    ),
-                  );
+                  questionNumber++;
                 });
               },
             ),
