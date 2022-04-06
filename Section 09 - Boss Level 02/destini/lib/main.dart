3d1581a3e608ecf095821ff8263984cb501ecfbf
Rodrigogzmn6 <rodrigoguzman6796@gmail.com>
2022-04-06 19:05:36 -0300

    Step 24 completed

--- Section 09 - Boss Level 02/destini/lib/main.dart
+++ Section 09 - Boss Level 02/destini/lib/main.dart
@@ -51,8 +51,8 @@ class _StoryPageState extends State<StoryPage> {
                 flex: 2,
                 child: TextButton(
                   onPressed: () {
-                    //Choice 1 made by user.
-                    //TODO: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
+                    storyBrain.nextStory(1);
+                    setState(() {});
                   },
                   style: TextButton.styleFrom(backgroundColor: Colors.blue),
                   child: Text(
@@ -73,8 +73,8 @@ class _StoryPageState extends State<StoryPage> {
                 //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
                 child: TextButton(
                   onPressed: () {
-                    //Choice 2 made by user.
-                    //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
+                    storyBrain.nextStory(2);
+                    setState(() {});
                   },
                   style: TextButton.styleFrom(backgroundColor: Colors.red),
                   child: Text(
@@ -93,7 +93,5 @@ class _StoryPageState extends State<StoryPage> {
     );
   }
 }
-
-//TODO: Step 24 - Run the app and try to figure out what code you need to add to this file to make the story change when you press on the choice buttons.
 
 //TODO: Step 29 - Run the app and test it against the Story Outline to make sure you've completed all the steps. The code for the completed app can be found here: https://github.com/londonappbrewery/destini-challenge-completed/
